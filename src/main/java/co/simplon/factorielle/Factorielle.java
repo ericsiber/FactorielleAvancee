d31edb734e812827e931c79790052d12d1ecb502
Eric Siber <eric.siber@gmail.com>
2017-03-07 10:04:13 +0100

    Etape 3

--- src/main/java/co/simplon/factorielle/Factorielle.java
+++ src/main/java/co/simplon/factorielle/Factorielle.java
@@ -1,21 +1,27 @@
 package co.simplon.factorielle;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * Factorielle
  */
-public class Factorielle 
-{
-    public Factorielle() {
-    		super();
-    }
-    
-    public long calculer(long n) {
-    		if (n > 1) {
-    			return n * calculer(n-1);
-    		} else if (n < 0) {
-    			throw new IllegalArgumentException();
-    		}
-    		return n;
-    }
+public class Factorielle {
+	private static final Logger LOGGER = LoggerFactory
+			.getLogger(Factorielle.class);
+
+	public Factorielle() {
+		super();
+	}
+
+	public long calculer(long n) {
+		LOGGER.info("Calcul de !" + n);
+		if (n > 1) {
+			return n * calculer(n - 1);
+		} else if (n < 0) {
+			throw new IllegalArgumentException();
+		}
+		return n;
+	}
 
 }
